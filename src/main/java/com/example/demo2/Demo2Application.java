5ea8fa8e857bd96ccb14ba40178c4abbce537c8f
mengang-sys <377532225@qq.com>
2020-02-16 15:27:35 +0800

    【】

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo2Application {
 
     public static void main(String[] args) {
-        System.out.println("    ");
+        System.out.println("  ");
         SpringApplication.run(Demo2Application.class, args);
     }
 
