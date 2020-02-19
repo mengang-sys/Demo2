5cdf876363f50f431ca32aa73c7ced4a1767c69a
mengang-sys <377532225@qq.com>
2020-02-19 16:40:58 +0800

    [Meng].2222

--- src/main/java/com/example/demo2/Meng.java
+++ src/main/java/com/example/demo2/Meng.java
@@ -4,5 +4,6 @@ public class Meng {
     public static void main(String[] args) {
         System.out.println("0000");
         System.out.println("1111");
+        System.out.println("2222");
     }
 }
