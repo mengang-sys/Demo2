ed8f9083115fd46fd37204b49ecf5448a98aa983
mengang-sys <377532225@qq.com>
2020-02-19 15:42:54 +0800

    修改Yang为0000

--- src/main/java/com/example/demo2/Liu.java
+++ src/main/java/com/example/demo2/Liu.java
@@ -5,7 +5,7 @@ public class Liu  {
     public static void main(String[] args) {
         System.out.println("Liu");
         System.out.println("Meng");
-        System.out.println("Yang");
+        System.out.println("0000");
         System.out.println("11111");
         System.out.println("44444");
         System.out.println("55555");
